f28be0df35332b4b78fbcf647d0959a206244886
ბექა ჯაიანი <121308857+JAIANI@users.noreply.github.com>
2023-03-13 21:31:15 +0300

    add renderCountries

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+// delete button after click
 function deleteButton() {
   var button = document.getElementById("delete-btn");
   button.parentNode.removeChild(button);
 }
+// refresh page after click
 function refresh() {
   window.location.reload();
 }
-
+// add image
 const image = document.createElement("img");
-document.body.appendChild(image);
+document.body.prepend(image);
 image.src = " https://picsum.photos/1400/800";
 image.classList.add("photos");
 console.log(image);
+// // change title after click
